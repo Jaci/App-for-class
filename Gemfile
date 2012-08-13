74531ebe1e574526722de30e20f63443b0ce919a
Jaci <jacischneider@gmail.com>
2012-08-12 21:55:51 -0500

    Editing forms

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ gem 'rails', '3.2.6'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
+gem 'chosen-rails'
 gem 'devise'
 
 group :development do
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,10 @@ class HomeController < ApplicationController
 
 before_filter :authenticate_user!
 
+def index
+
+       @consultants = Consultant.all
+       @contacts = Contact.all
+   end
+
 end
